e3dd3fe6f6a4bd9105d9fbc9e24b846ee66ec19c
Jadenir Souza <jadenir.p.s@gmail.com>
2020-02-26 19:57:47 -0300

    Correção da rota para produto, quando acessado pela paginade login por conta da validação de acesso.

--- QuickBuy/QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy/QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../../modelo/usuario';
-import { Router } from "@angular/router"
+import { Router, ActivatedRoute } from "@angular/router"
+import { state } from '@angular/animations';
 
 @Component({
     selector: "app-login",
@@ -8,17 +9,23 @@ import { Router } from "@angular/router"
     styleUrls: ["./login.component.css"]
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
     public usuario;
+    public returnUrl: string;
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private activatedRouter: ActivatedRoute) {
+
+    }
+
+    ngOnInit(): void {
+        this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
         this.usuario = new Usuario();
     }
 
     entrar() {
         if (this.usuario.email == "jadenir.p.s@gmail.com" && this.usuario.senha == "asdf") {
             sessionStorage.setItem("usuario-autenticado", "1");
-            this.router.navigate(['/']);
+            this.router.navigate([this.returnUrl]);
         }
     }
 }
